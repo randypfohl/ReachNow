7780b63ab64ca2c446055700dbf7094ddd4a980e
Education <randypfohl@gmail.com>
2018-09-27 22:18:42 -0700

    cleaning up

--- src/ReachNowChallenge.java
+++ src/ReachNowChallenge.java
@@ -18,9 +18,7 @@ public class ReachNowChallenge {
 	public Character[][] getFrequencyArray(){
 		// Create frequency map
 		Map<Character, Integer> map = new HashMap<Character, Integer>();
-		
 		for(char x : input.toCharArray()) {
-			
 			if(!map.containsKey(x)) {
 				map.put(x, 1);
 			}
@@ -28,7 +26,6 @@ public class ReachNowChallenge {
 				map.put(x, map.get(x) + 1);
 			}
 		}
-		
 		// Return Sorted Array
 		return getSortedArray(map);
 	}
@@ -41,16 +38,13 @@ public class ReachNowChallenge {
 		//map the hashmap to linear object
 		List<Map.Entry<Character, Integer>> list = new ArrayList<Map.Entry<Character, Integer>>(map.entrySet());
 		Collections.sort(list, new Comparator<Map.Entry<Character, Integer>>() {
-
 			public int compare(Entry<Character, Integer> o1, Entry<Character, Integer> o2) {
-				
 				return o2.getValue().compareTo(o1.getValue());
 			}
 	    });
 		
 		// map sorted values to 2d array
 			for(int i = 0; i < list.size(); i++) {
-				
 				solution[i][0] = list.get(i).getKey();
 				solution[i][1] = Character.forDigit(list.get(i).getValue(), 10);
 			}
