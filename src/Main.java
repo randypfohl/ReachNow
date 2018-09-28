f597ae005d85c59bd769454a3bbb1d1902f7d5f2
Education <randypfohl@gmail.com>
2018-09-27 20:42:47 -0700

    Reach Now

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		String test = "aoNHQATMNLWC4vMdGgQpqMgbit";
+		
+		ReachNowChallenge challenge = new ReachNowChallenge(test);
+		
+		Character[][] result = challenge.getFrequencyArray(test);
+		
+		for(Character[] arr : result) {
+			System.out.println(Character.valueOf(arr[0]) + "," + arr[1]);
+		}
+	}
+
+}
---
+++ src/ReachNowChallenge.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+public class ReachNowChallenge {
+	
+	String array;
+	
+	public ReachNowChallenge(String array) {
+		this.array = array;
+	}
+	
+	public Character[][] getFrequencyArray(String array){
+		// Create frequency map
+		Map<Character, Integer> map = new HashMap<Character, Integer>();
+		
+		for(char x : array.toCharArray()) {
+			
+			if(!map.containsKey(x)) {
+				map.put(x, 1);
+			}
+			else {
+				map.put(x, map.get(x) + 1);
+			}
+		}
+		
+		// Return Sorted Array
+		return getSortedArray(map);
+	}
+	
+
+	private Character[][] getSortedArray(Map<Character, Integer> map){
+		//initialize 2d array
+		Character[][] solution = new Character[map.size()][2];
+		
+		//map the hashmap to linear object
+		List<Map.Entry<Character, Integer>> list = new ArrayList<Map.Entry<Character, Integer>>(map.entrySet());
+		Collections.sort(list, new Comparator<Map.Entry<Character, Integer>>() {
+
+			public int compare(Entry<Character, Integer> o1, Entry<Character, Integer> o2) {
+				
+				return o2.getValue().compareTo(o1.getValue());
+			}
+	    });
+		
+		// map sorted values to 2d array
+			for(int i = 0; i < list.size(); i++) {
+				
+				solution[i][0] = list.get(i).getKey();
+				solution[i][1] = Character.forDigit(list.get(i).getValue(), 10);
+			}
+			
+			return solution;
+		}
+}
